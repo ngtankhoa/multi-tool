fd97bb3e82a13ea2d0bc1d40fb42d3a0042ebb32
John Nguyen <john.nguyen@legato.co>
2023-05-25 00:09:42 +0700

    better handling async

--- app/get-content-from-url/page.js
+++ app/get-content-from-url/page.js
@@ -1,71 +1,66 @@
 'use client'
 
 import { useEffect, useRef, useState } from 'react'
+import cn from 'classnames'
 
 export default function GetFromURL() {
   const [data, setData] = useState([])
   const [errors, setErrors] = useState([])
+  const [loading, setLoading] = useState(false)
   const textAreaRef = useRef()
 
-  useEffect(() => {
-    // fetch(
-    //   `https://api.allorigins.win/get?url=${encodeURIComponent(
-    //     'https://limosa.vn/sua-may-loc-nuoc-barrier-bi-ri-nuoc/'
-    //   )}`
-    // )
-    //   .then((response) => {
-    //     if (response.ok) return response.json()
-    //     throw new Error('Network response was not ok.')
-    //   })
-    //   .then((data) => {
-    //     console.log(data.contents)
-    //     const doc = new DOMParser().parseFromString(data.contents, 'text/xml')
-    //     const title = data.contents.match(/<title[^>]*>([^<]+)<\/title>/)[1]
-    //     const description = doc.querySelector('meta[name="description"]').getAttribute('content')
-    //     console.log({ title, description })
-    //     setData((curData) => [...curData, { title, description }])
-    //   })
-  }, [])
-
-  const handleGetTitleAndDescriptionFrom = (url) => {
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
-      .then((response) => {
-        if (response.ok) return response.json()
-        throw new Error('Network response was not ok.')
-      })
-      .then((data) => {
-        console.log(data.contents)
-        const doc = new DOMParser().parseFromString(data.contents, 'text/xml')
-        const title = data.contents.match(/<title[^>]*>([^<]+)<\/title>/)[1]
-        const description = doc.querySelector('meta[name="description"]').getAttribute('content')
-        console.log(doc)
-        console.log({ title, description })
-        setData((curData) => [...curData, { title, description }])
-      })
-      .catch((err) => {
-        setErrors((curUrl) => [...curUrl, url])
-      })
-  }
-
-  const handleGetResult = () => {
+  const handleGetResult = async () => {
     setData([])
     setErrors([])
     const urlList = textAreaRef.current.value.split(/\r?\n|\r|\n/g)
-    console.log(urlList)
-    urlList
-      .filter((item) => item.length !== 0)
-      .forEach((url) => {
-        handleGetTitleAndDescriptionFrom(url)
-      })
+
+    setLoading(true)
+    const resultArray = await Promise.allSettled(
+      urlList
+        .filter((item) => item.length !== 0)
+        .map((url) =>
+          fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
+            .then((response) => {
+              if (response.ok) return response.json()
+              throw new Error('Network response was not ok.')
+            })
+            .then((data) => ({ isValid: true, data: data.contents }))
+            .catch((err) => ({ isValid: false, data: url }))
+        )
+    )
+    setLoading(false)
+
+    setErrors(resultArray.filter((result) => !result.value.isValid).map((result) => result.value.data))
+    setData(
+      resultArray
+        .filter((result) => result.value.isValid)
+        .map((result) => {
+          const doc = new DOMParser().parseFromString(result.value.data, 'text/xml')
+          const title = result.value.data.match(/<title[^>]*>([^<]+)<\/title>/)[1]
+          const description = doc.querySelector('meta[name="description"]').getAttribute('content')
+          return { title, description }
+        })
+    )
+
+    console.log(resultArray)
   }
 
   return (
     <div>
       <div className='flex flex-col items-center gap-4 pt-4'>
         <p>Hi mini Sài Gòn : )</p>
-        <textarea ref={textAreaRef} rows='7' className='text-black sm:w-3/4 lg:w-1/2 ' />
-        <button onClick={handleGetResult} className='bg-white text-black py-2 px-4 rounded-md'>
-          Lấy title và description
+        <textarea ref={textAreaRef} rows='7' className='text-black w-10/12 sm:w-3/4 ' />
+        <button
+          disabled={loading}
+          onClick={handleGetResult}
+          className='bg-white text-black py-2 px-4 rounded-md disabled:bg-slate-500'
+        >
+          <div className={cn(loading && 'invisible', 'relative')}>
+            Lấy title và description
+            <div className={cn('absolute top-0 left-1/2 -translate-x-1/2', loading ? 'visible' : 'invisible')}>
+              đợi chút...
+            </div>
+          </div>
         </button>
       </div>
       {errors.length !== 0 && (
